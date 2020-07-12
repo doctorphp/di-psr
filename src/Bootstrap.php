d559136f1485ad51f5c228d03f7963064a3444f8
Pavel Janda <me@paveljanda.com>
2020-07-12 22:42:04 +0200

    Added files from sandbox

---
+++ src/Bootstrap.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @author Pavel Janda <me@paveljanda.com>
+ * @copyright Copyright (c) 2020, Pavel Janda
+ */
+
+namespace Doctor\DI\PSR;
+
+use DI\Container;
+use DI\ContainerBuilder;
+use Whoops\Handler\PrettyPageHandler;
+use Whoops\Run;
+
+final class Bootstrap
+{
+
+	private ContainerBuilder $containerBuilder;
+	private bool $debugMode = false;
+	private ?string $cacheDir = null;
+
+
+	public function __construct()
+	{
+		$this->containerBuilder = new ContainerBuilder;
+
+		$this->addConfigFile(__DIR__ . '/psr-config.php');
+	}
+
+
+	public function setDebugMode(bool $debugMode = true): void
+	{
+		if ($debugMode) {
+			$whoops = new Run;
+			$whoops->pushHandler(new PrettyPageHandler);
+			$whoops->register();
+		}
+
+		$this->debugMode = $debugMode;
+	}
+
+
+	public function setCacheDir(string $cacheDir): void
+	{
+		$this->cacheDir = rtrim($cacheDir, '/');
+		$routerCacheDir = $this->cacheDir . '/router';
+
+		if (!is_dir($routerCacheDir)) {
+			mkdir($routerCacheDir, 0777, true);
+
+			if (!is_dir($routerCacheDir)) {
+				throw new \RuntimeException(
+					sprintf('Could not created cache directory %s', $routerCacheDir)
+				);
+			}
+		}
+	}
+
+
+	public function addConfigFile(string $configFile): void
+	{
+		$this->containerBuilder->addDefinitions($configFile);
+	}
+
+
+	public function createContainer(): Container
+	{
+		if ($this->cacheDir === null) {
+			throw new \RuntimeException(
+				sprintf('Please set a cache directory using %s::setCacheDir()', self::class)
+			);
+		}
+
+		$this->containerBuilder->addDefinitions([
+			'cacheDir' => $this->cacheDir,
+			'debugMode' => $this->debugMode,
+		]);
+
+		if ($this->debugMode === false) {
+			$this->containerBuilder->enableCompilation($this->cacheDir . '/di');
+			$this->containerBuilder->writeProxiesToFile(true, $this->cacheDir . '/proxies');
+		}
+
+		return $this->containerBuilder->build();
+	}
+}
