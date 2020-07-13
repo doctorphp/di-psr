c8edb8e1f725fc0298c069ea436d91802aaf609c
Pavel Janda <me@paveljanda.com>
2020-07-13 09:45:14 +0200

    config: added ControllerProvider (ControllerProviderInterface)

--- src/psr-config.php
+++ src/psr-config.php
@@ -2,8 +2,10 @@
 
 declare(strict_types=1);
 
+use Doctor\DI\PSR\Controller\ControllerProvider;
 use Doctor\Http\RequestFactory;
 use Doctor\Rest\Application;
+use Doctor\Rest\Controller\ControllerProviderInterface;
 use Doctor\Rest\Route\Router;
 use Doctor\Rest\Route\RouterCache;
 use Psr\Container\ContainerInterface;
@@ -16,4 +18,7 @@ return [
 	Router::class => DI\autowire()->constructor(DI\get('cacheDir'), DI\get('debugMode')),
 	RouterCache::class => DI\autowire()->constructor(DI\get('cacheDir')),
 	Application::class => DI\autowire()->constructor(DI\get('debugMode')),
+	ControllerProviderInterface::class => function(ContainerInterface $container): ControllerProviderInterface {
+		return $container->get(ControllerProvider::class);
+	},
 ];
